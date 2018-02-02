c8e3d276d6ddd541b954e3df14923a869eb2e1d5
Arthur Dassier <arthur.dassier@epitech.eu>
2018-02-02 19:49:58 +0100

    Better AI

--- srcs/algo.c
+++ srcs/algo.c
@@ -19,13 +19,37 @@ int calc(int *mariebd)
 	return (somme);
 }
 
+int calc_dec(int *mariebd)
+{
+	int	checker = 0;
+	int	somme = 0;
+	int	i = 0;
+
+	while (mariebd[i] != -1) {
+		somme = somme + mariebd[i];
+		++i;
+		if (mariebd[i] > 0)
+			++checker;
+	}
+	if ((checker == 1 && somme > 1) || (checker == 2 && somme == 2))
+		return (2);
+	if (checker == 3 && somme == 4)
+		return (3);
+	return (somme);
+}
+
 void params_create(int *mariebd, char **map, int max)
 {
 	int	alum = 0;
 	int	save = 0;
 	int	i = 0;
+	int	check = 0;
 
-	while (calc(mariebd) != 0) {
+	if (calc_dec(mariebd) == 2)
+		check = 1;
+	if (calc_dec(mariebd) == 3)
+		check = 2;
+	while (calc(mariebd) != check) {
 		if (i == 0 && alum == 0)
 		save = mariebd[i];
 		--mariebd[i];
